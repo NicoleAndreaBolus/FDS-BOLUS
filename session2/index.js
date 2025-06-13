e0771cc82f7cd46debdbae778cb6dda03df2b030
NicoleAndreaBolus <nagbolus.student@ua.edu.ph>
2025-06-13 08:19:02 +0800

    Initial commit

---
+++ session2/index.js
@@ -0,0 +1,60 @@
+// SESSION 2 - 6/13/2025
+
+// console.log("Hello World!")
+
+// JS BASIC FUNCTION/S
+
+function sayHello(){
+    console.log("Hello World!");
+}
+
+// function calling  // Invocation
+
+sayHello(); // This is a function call or invocation
+
+function completeName(){
+    // Local Scope Variable -> fullName
+    let fullName = "Juan Dela Cruz";
+    console.log(fullName);
+}
+
+completeName(); 
+// console.log(fullName); -> error: fullName is not defined
+
+
+// Global and Local Scope Variables
+
+let age = 20; // Global Scope Variable - accessible anywhere in the script
+
+{
+    // Local Scope Variable
+    const pi = 3.14;
+    console.log(age); // Accessing the global variable inside the curly braces
+}
+
+// Accessing the variable inside the curly braces
+// console.log(pi); -> error: pi is not defined
+
+let studentName = "Mary Poppins";
+
+function getName(){
+    let age = 50;
+    console.log(studentName + " age is " + age); // Accessing the global variable inside the function
+}
+
+getName(); // Calling the function to see the output / Invocation
+
+// Function with Alert
+
+function helloAlert(){
+    alert("Hello World!");
+}
+
+// It is not necessary to call the function in the js file. We can also call a function in the Console Panel of the browser
+
+// Function with Prompt()
+
+function enterName(){
+    let inputName = prompt("Please enter your name:");
+    console.log("Hello there, " + inputName + "!");
+}
