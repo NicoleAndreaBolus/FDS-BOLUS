70ef337ebe5a3e706940249e79bccd6eb9f59302
NicoleAndreaBolus <nagbolus.student@ua.edu.ph>
2025-07-07 11:07:15 +0800

    Added changes

--- session11/index.js
+++ session11/index.js
@@ -15,25 +15,28 @@ const app = http.createServer((request, response) => {
     // Endpoint Creation via Node Server
     // localhost:4000/greeting
 
+
+    // Greeting Endpoint
     if (request.url == "/greeting") {
         response.end("Hello, World!");
-
+    // Home Page Endpoint
     } else if (request.url == "/homepage") {
         response.end("Welcome to the Homepage, User!");
-
+    // About Page Endpoint
     } else if (request.url == "/about") {
         response.writeHead(200);
         response.end("This is the About Page.");
-
+        // Items Endpoint
     } else if (request.url == "/items" && request.method == "GET") {
         response.writeHead(200);
         response.end(JSON.stringify(items));
-
+        // Add Item Endpoint
     } else if (request.url == "/items/add" && request.method == "POST") {
         response.writeHead(201);
         items.push("Desktop"); // Simulating adding a new item
         response.end("Item added successfully");
 
+        // Login Endpoint
     } else if (request.url == "/login" && request.method == "GET") {
         if (users.length > 0) {
             response.writeHead(200);
@@ -43,8 +46,9 @@ const app = http.createServer((request, response) => {
             response.end("Login failed. No users found.");
         }
 
+        // Registration Endpoint
     } else if (request.url == "/register" && request.method == "POST") {
-        if (users.length < 1) {
+        if (users.length < 10) {
             response.writeHead(201);
             users.push("Charlie"); // Simulating user registration
             response.end("User registered successfully.");
@@ -52,7 +56,7 @@ const app = http.createServer((request, response) => {
             response.writeHead(400);
             response.end("User registration failed. Maximum users reached.");
         }
-
+        // Delete Item Endpoint
     } else if (request.url == "/items/delete" && request.method == "DELETE") {
         if (items.length > 0) {
             response.writeHead(200);
@@ -62,7 +66,7 @@ const app = http.createServer((request, response) => {
             response.writeHead(400);
             response.end("Database is already empty.");
         }
-
+        // Default Case for 404
     } else {
         response.writeHead(404);
         response.end("ERROR 404: Page Not Found!");
